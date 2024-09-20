44f08b8b6576e7dd68ef4a0154e01b3bb6e461d3
Samarth Jain <99988982+samarth-jain28@users.noreply.github.com>
2024-09-20 13:03:30 +0000

    added dynamic programming

---
+++ dynamicProgramming/Main.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[]args){
+        int w = 7;
+        int n = 4;
+        int[]wt={1,3,4,5};
+        int[]val={1,4,5,7};
+        System.out.println(knapsack.solve01Knapsack(wt, val, w, n));
+    }
+}
---
+++ dynamicProgramming/knapsack.java
@@ -0,0 +1,15 @@
+
+class knapsack{
+    public static int solve01Knapsack(int[]wt, int[]val, int w, int n){
+        if(w==0 || n==0){
+            return 0;
+        }
+        if(wt[n-1]<=w){
+            return Math.max(val[n-1]+solve01Knapsack(wt, val, w-wt[n-1],n-1), solve01Knapsack(wt, val, w, n-1));
+        }
+        else if(wt[n-1]>w){
+            return solve01Knapsack(wt, val, w, n-1);
+        }
+        return 0;
+    }
+}
