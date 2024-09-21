1989326917bebaefdd66068e3114b6cba80be409
Samarth Jain <99988982+samarth-jain28@users.noreply.github.com>
2024-09-21 01:03:50 +0000

    subset sum

--- dynamicProgramming/Main.java
+++ dynamicProgramming/Main.java
@@ -1,9 +1,15 @@
+import java.util.Arrays;
 class Main{
     public static void main(String[]args){
-        int w = 7;
-        int n = 4;
-        int[]wt={1,3,4,5};
-        int[]val={1,4,5,7};
-        System.out.println(knapsack.solve01Knapsack(wt, val, w, n));
+        // int w = 7;
+        // int n = 4;
+        // int[]wt={1,3,4,5};
+        // int[]val={1,4,5,7};
+        // System.out.println(knapsack.solve01Knapsack(wt, val, w, n));
+        subsetSum s = new subsetSum();
+        int ar[] = {1,3,9};
+        System.out.println(s.solveSubsetSum(ar, 4));
+        
+
     }
 }
---
+++ dynamicProgramming/subsetSum.java
@@ -0,0 +1,26 @@
+public class subsetSum{
+    private boolean t[][];
+
+    public boolean solveSubsetSum(int[]ar, int sum){
+        int n = ar.length;
+        t = new boolean[n+1][sum+1];
+        for(int i=1;i<sum+1;i++){
+            t[0][i] = false;
+        }
+        for(int i=0;i<n+1;i++){
+            t[i][0] = true;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(ar[i-1]==j){
+                    t[i][j] = t[i][j-ar[i-1]]||t[i-1][j];
+                }else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+
+        return t[n][sum];
+    }
+}
