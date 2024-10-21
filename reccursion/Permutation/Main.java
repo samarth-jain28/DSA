9d023fdd4b4054eb8beb85234200a1c8aa176b06
Samarth Jain <99988982+samarth-jain28@users.noreply.github.com>
2024-10-21 11:58:46 +0000

    permutation

--- reccursion/Permutation/Main.java
+++ reccursion/Permutation/Main.java
@@ -6,28 +6,39 @@ import java.util.ArrayList;
 
 public class Main {
 
-    static private void PrintPermutation(List<Integer> ar3, int n){
-        if(n==0){
-            System.out.println(ar3);
+    private static boolean PrintPermutation(List<Integer>arr, List<Integer>ar, int itr){
+        if(arr.equals(ar)){
+            System.out.println(ar);
+            return true;
         }
-        
+        if(itr == ar.size()){
+            System.out.println(ar);
+            return false;
+        }
+            List<Integer>ar2 = new ArrayList<>(ar);
+            for(int i=itr; i<ar2.size(); i++){
+                int temp = ar2.get(itr);
+                ar2.set(itr, ar2.get(i));
+                ar2.set(i, temp);
+                PrintPermutation(arr, ar2, itr+1);
+            }
+        return false;
     }
     public static void main(String[] args) {
         List<Integer>ar = new ArrayList<>();
         Integer[]ar2={1,2,3};
         List<Integer> ar3 = new ArrayList<>(Arrays.asList(ar2));
-        List<Integer> ar4 = new ArrayList<>(ar3);
+        Integer[]ar5={1,3,2};
+        List<Integer> ar4 = new ArrayList<>(Arrays.asList(ar5));
 
         //System.out.println(ar);
         //System.out.println(ar2);
         //System.out.println(ar3);
         //System.out.println(ar4);
         // ar4.add(2);
-        ar3.add(2);
-        System.out.println(ar4);
-
-        PrintPermutation(ar3, ar3.size());
-        System.out.println(ar3);
+        //ar3.add(2);
+        //System.out.println(ar4);
+        System.out.println(PrintPermutation(ar4, ar3, 0));
     }
 
 }
