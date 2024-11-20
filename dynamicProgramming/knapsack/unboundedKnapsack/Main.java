32c5c0d9a7f1b8cb0a4bead05475141858fc28b8
Samarth Jain <99988982+samarth-jain28@users.noreply.github.com>
2024-11-20 11:56:44 +0000

    rod cutting problem

--- dynamicProgramming/knapsack/unboundedKnapsack/Main.java
+++ dynamicProgramming/knapsack/unboundedKnapsack/Main.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[]args){
-        System.out.println("Hello");
 
+        Integer[]length = {1,3,4,2,6,9};
+        Integer[]prices = {2,5,6,3,9,15};
+        int N = 200;
+
+        List<Integer>len = new ArrayList<>(Arrays.asList(length));
+        List<Integer>price = new ArrayList<>(Arrays.asList(prices));
         
+        // System.out.println("Ans is: "+rodCutting.solve(len, price, N, len.size()));
+        // System.out.println(rodCutting.count);
+
+        // System.out.println("Ans is: "+rodCutting.solveTopDown(len, price, N, len.size()));
+        // System.out.println(rodCutting.count2);
+
+        System.out.println("Ans is: "+rodCutting.solveMemoise(len, price, N, len.size()));
+        // System.out.println(rodCutting.count2);
+
     }
 }
--- dynamicProgramming/knapsack/unboundedKnapsack/rodCutting.java
+++ dynamicProgramming/knapsack/unboundedKnapsack/rodCutting.java
@@ -1,15 +1,58 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 class rodCutting {
+    static int count = 0;
+    static int count2 = 0;
     public static int solve(List<Integer>len, List<Integer>price, int N, int n){
         if(n == 0){
             return 0;
         }
-        if(N<len.get(n-1)){
+        if(N == 0){
+            return 0;
+        }
+        count++;
+        if(N>=len.get(n-1)){
             return Math.max( price.get(n-1)+solve(len, price, N-len.get(n-1), n),
                    solve(len, price, N, n-1) );
         }else{
             return solve(len, price, N, n-1);
         }
-    }   
+    }
+    
+    public static int solveTopDown(List<Integer>len, List<Integer>price, int N, int n){
+        int [][]dp = new int[n+1][N+1];
+
+        for(int i=0;i<N+1;i++){
+            dp[0][i]=0;
+        }
+        for(int i=0;i<n+1;i++){
+            dp[i][0]=0;
+        }
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<N+1;j++){
+                if(len.get(i-1)<=j){
+                    dp[i][j] = Math.max(price.get(i-1)+dp[i][j-len.get(i-1)],
+                        dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+                count2++;
+            }
+        }
+
+        return dp[n][N];
+
+    }
+
+    static List<List<Integer>>dp = new ArrayList<>(Collections.nCopies(5, new ArrayList<>(Collections.nCopies(10, -1))));
+    public static int solveMemoise(List<Integer>len, List<Integer>price, int N, int n){
+        for(List<Integer> i : dp){
+            System.out.println(i);
+        }
+        System.out.println(dp.size());
+        return 0;
+    }
 }
